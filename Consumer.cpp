4f825701856cb8386c2401115c37cc3c207c04fc
Umer Nalla <umer.nalla@refinitiv.com>
2021-10-21 12:09:58 +0100

    Update Consumer.cpp

--- Consumer.cpp
+++ Consumer.cpp
@@ -2,7 +2,7 @@
 // *|            This source code is provided under the Apache 2.0 license      --
 // *|  and is provided AS IS with no warranty or guarantee of fit for purpose.  --
 // *|                See the project's LICENSE.md for details.                  --
-// *|           Copyright Thomson Reuters 2017. All rights reserved.            --
+// *|           Copyright Refinitiv 2021. All rights reserved.            --
 // *|     It has not been tested for a usage in production environments.        --
 ///*|-----------------------------------------------------------------------------
 
