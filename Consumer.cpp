0dea6955521bcc3d5b0153e80c86fea4ca8ecdda
Umer Nalla <umer.nalla@refinitiv.com>
2021-10-21 12:10:24 +0100

    Update Consumer.cpp

--- Consumer.cpp
+++ Consumer.cpp
@@ -11,8 +11,8 @@
 #include <vector>
 #include <algorithm>
 
-using namespace thomsonreuters::ema::access;
-using namespace thomsonreuters::ema::rdm;
+using namespace refinitiv::ema::access;
+using namespace refinitiv::ema::rdm;
 using namespace std;
 
 EmaString hostAndPort;
